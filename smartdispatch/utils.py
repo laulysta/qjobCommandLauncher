4f112367e10774a8c212f1fde9f7ec0756df81bf
Marc-Alexandre Côté <marc.cote.19@gmail.com>
2015-12-14 18:23:34 -0500

    Fixed UFT8 problems since Python3 handles strings differently

--- smartdispatch/utils.py
+++ smartdispatch/utils.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 
 import re
+import binascii
 import hashlib
 import unicodedata
 import json
@@ -37,13 +38,13 @@ def yes_no_prompt(query, default=None):
 
 def chunks(sequence, n):
     """ Yield successive n-sized chunks from sequence. """
-    for i in xrange(0, len(sequence), n):
+    for i in range(0, len(sequence), n):
         yield sequence[i:i + n]
 
 
 def generate_uid_from_string(value):
     """ Create unique identifier from a string. """
-    return hashlib.sha256(value).hexdigest()
+    return hashlib.sha256(value.encode()).hexdigest()
 
 
 def slugify(value):
@@ -56,7 +57,12 @@ def slugify(value):
     ---------
     https://github.com/django/django/blob/1.7c3/django/utils/text.py#L436
     """
-    value = unicodedata.normalize('NFKD', unicode(value, "UTF-8")).encode('ascii', 'ignore').decode('ascii')
+    try:
+        value = unicode(value, "UTF-8")
+    except NameError:
+        pass  # In Python 3 all strings are already unicode.
+
+    value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore').decode('ascii')
     value = re.sub('[^\w\s-]', '', value).strip().lower()
     return str(re.sub('[-\s]+', '_', value))
 
@@ -64,7 +70,8 @@ def slugify(value):
 def encode_escaped_characters(text, escaping_character="\\"):
     """ Escape the escaped character using its hex representation """
     def hexify(match):
-        return "\\x{0}".format(match.group()[-1].encode("hex"))
+        # Reference: http://stackoverflow.com/questions/18298251/python-hex-values-to-convert-to-a-string-integer
+        return "\\x" + binascii.hexlify(match.group()[-1].encode()).decode()
 
     return re.sub(r"\\.", hexify, text)
 
@@ -75,7 +82,7 @@ def decode_escaped_characters(text):
         return ''
 
     def unhexify(match):
-        return match.group()[2:].decode("hex")
+        return binascii.unhexlify(match.group()[2:]).decode()
 
     return re.sub(r"\\x..", unhexify, text)
 
