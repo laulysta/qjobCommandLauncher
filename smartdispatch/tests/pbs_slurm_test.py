e8e6ec0f67ec4e1465f68db2f0414261fe6dc0b1
Adrien Ali Taiga <adrien.alitaiga@gmail.com>
2017-09-01 17:22:07 -0400

    Fixed naccelerators issue

--- smartdispatch/tests/pbs_slurm_test.py
+++ smartdispatch/tests/pbs_slurm_test.py
@@ -28,8 +28,6 @@ sbatch_string = """\
 #SBATCH --job-name=arrayJob
 #SBATCH --output=arrayJob_%A_%a.out
 #SBATCH --time=01:00:00
-#SBATCH --gres=gpu
-#SBATCH --constraint=gpu6gb
 {}
 
 ######################
@@ -48,17 +46,17 @@ class TestSlurm(unittest.TestCase):
         for file_name in (glob('*.out') + ["test.pbs"]):
             os.remove(file_name)
 
-    def _test_param(self, param_array, com, flag, string=pbs_string):
-        for param in param_array:
-            command = pbs_string.format(
-                string.format(com.format(param))
+    def _test_param(self, param_array, command, flag, string=pbs_string, output_array=None):
+        output_array = output_array or param_array
+        for param, output in zip(param_array, output_array):
+            com = pbs_string.format(
+                string.format(command.format(param))
             )
             with open("test.pbs", "w") as text_file:
-                text_file.write(command)
+                text_file.write(com)
             process = Popen("sbatch test.pbs", stdout=PIPE, stderr=PIPE, shell=True)
             stdout, _ = process.communicate()
             stdout = stdout.decode()
-            print(stdout)
             self.assertIn("Submitted batch job", stdout)
             job_id = stdout.split(" ")[-1].strip()
 
@@ -66,8 +64,7 @@ class TestSlurm(unittest.TestCase):
             process = Popen("squeue -u $USER -j {} -O {}".format(job_id, flag), stdout=PIPE, stderr=PIPE, shell=True)
             stdout, _ = process.communicate()
             job_params = [c.strip() for c in stdout.decode().split("\n")[1:] if c != '']
-            # import ipdb; ipdb.set_trace()
-            self.assertSequenceEqual(job_params, [param for _ in range(len(job_params))])
+            self.assertSequenceEqual(job_params, [output for _ in range(len(job_params))])
 
     def test_priority(self):
         self._test_param(
@@ -79,15 +76,16 @@ class TestSlurm(unittest.TestCase):
 
     def test_gres(self):
         self._test_param(
-            ['k80'],
+            ["1", "2"],
             "#PBS -l naccelerators={}",
             "gres",
-            pbs_string
+            pbs_string,
+            ["gpu:1", "gpu:2"]
         )
 
     def test_memory(self):
         self._test_param(
-            ['2G', '4G'],
+            ["2G", "4G"],
             "#PBS -l mem={}",
             "minmemory",
             pbs_string
