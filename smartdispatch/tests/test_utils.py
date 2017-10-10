93a4a3a39ef553d1c23805b93451734e3de4d2e1
Adrien Ali Taiga <adrien.alitaiga@gmail.com>
2017-10-09 20:35:15 -0400

    Updated tests using mock

--- smartdispatch/tests/test_utils.py
+++ smartdispatch/tests/test_utils.py
@@ -1,11 +1,16 @@
 # -*- coding: utf-8 -*-
 import unittest
-
-from smartdispatch import utils
-
+try:
+    from mock import patch
+    import mock
+except ImportError:
+    from unittest.mock import patch
+    import unittest.mock
 from nose.tools import assert_equal, assert_true
 from numpy.testing import assert_array_equal
+import subprocess
 
+from smartdispatch import utils
 
 class PrintBoxedTests(unittest.TestCase):
 
@@ -49,3 +54,40 @@ def test_slugify():
 
     for arg, expected in testing_arguments:
         assert_equal(utils.slugify(arg), expected)
+
+command_output = """\
+Server             Max   Tot   Que   Run   Hld   Wat   Trn   Ext   Com Status
+----------------   ---   ---   ---   ---   ---   ---   ---   ---   --- ----------
+gpu-srv1.{}      0  1674   524   121    47     0     0    22   960 Idle
+"""
+
+slurm_command = """\
+   Cluster     ControlHost  ControlPort   RPC     Share GrpJobs       GrpTRES GrpSubmit MaxJobs       MaxTRES MaxSubmit     MaxWall                  QOS   Def QOS
+---------- --------------- ------------ ----- --------- ------- ------------- --------- ------- ------------- --------- ----------- -------------------- ---------
+      {}  132.204.24.224         6817  7680         1                                                                                           normal
+"""
+
+
+class ClusterIdentificationTest(unittest.TestCase):
+
+    def test_detect_cluster(self):
+        server_name = ["hades", "m", "guil", "helios", "hades"]
+        clusters = ["hades", "mammouth", "guillimin", "helios"]
+
+        for name, cluster in zip(server_name, clusters):
+            with patch('smartdispatch.utils.Popen') as mock_communicate:
+                mock_communicate.return_value.communicate.return_value = (command_output.format(name),)
+                self.assertEquals(utils.detect_cluster(), cluster)
+
+    # def test_detect_mila_cluster(self):
+    #     with patch('smartdispatch.utils.Popen') as mock_communicate:
+    #         mock_communicate.return_value.communicate.side_effect = OSError
+    #         self.assertIsNone(utils.detect_cluster())
+
+    def test_get_slurm_cluster_name(self):
+        clusters = ["graham", "cedar", "mila"]
+
+        for cluster in clusters:
+            with patch('smartdispatch.utils.Popen') as mock_communicate:
+                mock_communicate.return_value.communicate.return_value = (slurm_command.format(cluster),)
+                self.assertEquals(utils.get_slurm_cluster_name(), cluster)
