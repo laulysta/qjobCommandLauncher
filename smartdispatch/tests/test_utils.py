21df3dd449fd3e9a8ad2df80e2d75fadf9f5b2ff
Xavier Bouthillier <xavier.bouthillier@umontreal.ca>
2017-10-13 21:54:02 -0400

    Adapt PBS_WALLTIME for slurm
    Slurm does not have a equivalent environment variable set like
    PBS_WALLTIME. To avoid confusion, all variables PBS_WALLTIME are renamed
    to SBATCH_TIMELIMIT (the environment variable one would use to set --time
    with sbatch). As SBATCH_TIMELIMIT is not set automatically, we add it to
    the prolog to make it available to all commands and epilog.
    
    NOTE: PBS_WALLTIME is set in seconds, but we only have HH:MM:SS-like strings
    at the time of building the PBS file. We needed to add a
    walltime_to_seconds helper function to convert HH:MM:SS like strings
    into seconds, so that SBATCH_TIMELIMIT is set with seconds like
    PBS_WALLTIME.

--- smartdispatch/tests/test_utils.py
+++ smartdispatch/tests/test_utils.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import random
+
 import unittest
 try:
     from mock import patch
@@ -6,10 +8,66 @@ except ImportError:
     from unittest.mock import patch
 
 from nose.tools import assert_equal, assert_true
+
 from numpy.testing import assert_array_equal
 import subprocess
 
 from smartdispatch import utils
+
+
+class TestWalltimeToSeconds(unittest.TestCase):
+    def setUp(self):
+        self.format = dict(
+            days=random.randint(0, 10),
+            hours=random.randint(0, 23),
+            minutes=random.randint(0, 59),
+            seconds=random.randint(0, 59))
+
+    def _compute_seconds(self, days=0, hours=0, minutes=0, seconds=0):
+        return ((((days * 24) + hours) * 60) + minutes * 60) + seconds
+
+    def test_ddhhmmss(self):
+        seconds = utils.walltime_to_seconds(
+            "{days}:{hours}:{minutes}:{seconds}".format(**self.format))
+        self.assertEqual(seconds, self._compute_seconds(**self.format))
+
+    def test_hhmmss(self):
+        truncated_format = self.format.copy()
+        truncated_format.pop("days")
+
+        seconds = utils.walltime_to_seconds(
+            "{hours}:{minutes}:{seconds}".format(**truncated_format))
+        self.assertEqual(seconds, self._compute_seconds(**truncated_format))
+
+    def test_mmss(self):
+        truncated_format = self.format.copy()
+        truncated_format.pop("days")
+        truncated_format.pop("hours")
+
+        seconds = utils.walltime_to_seconds(
+            "{minutes}:{seconds}".format(**truncated_format))
+        self.assertEqual(seconds, self._compute_seconds(**truncated_format))
+
+    def test_ss(self):
+        truncated_format = self.format.copy()
+        truncated_format.pop("days")
+        truncated_format.pop("hours")
+        truncated_format.pop("minutes")
+
+        seconds = utils.walltime_to_seconds(
+            "{seconds}".format(**truncated_format))
+        self.assertEqual(seconds, self._compute_seconds(**truncated_format))
+
+    def test_too_much_columns(self):
+        with self.assertRaises(ValueError):
+            seconds = utils.walltime_to_seconds(
+                "1:{days}:{hours}:{minutes}:{seconds}".format(**self.format))
+
+    def test_with_text(self):
+        with self.assertRaises(ValueError):
+            seconds = utils.walltime_to_seconds(
+                "{days}hoho:{hours}:{minutes}:{seconds}".format(**self.format))
+
 
 class PrintBoxedTests(unittest.TestCase):
 
--- smartdispatch/utils.py
+++ smartdispatch/utils.py
@@ -1,10 +1,32 @@
-import re
 import hashlib
-import unicodedata
 import json
+import re
+import unicodedata
 
 from distutils.util import strtobool
 from subprocess import Popen, PIPE
+
+
+TIME_REGEX = re.compile(
+    "^(?:(?:(?:(\d*):)?(\d*):)?(\d*):)?(\d*)$")
+
+
+def walltime_to_seconds(walltime):
+    if not TIME_REGEX.match(walltime):
+        raise ValueError(
+            "Invalid walltime format: %s\n"
+            "It must be either DD:HH:MM:SS, HH:MM:SS, MM:SS or S" %
+            walltime)
+
+    split = walltime.split(":")
+
+    while len(split) < 4:
+        split = [0] + split
+
+    days, hours, minutes, seconds = map(int, split)
+
+    return ((((days * 24) + hours) * 60) + minutes * 60) + seconds
+
 
 def jobname_generator(jobname, job_id):
     '''Crop the jobname to a maximum of 64 characters.
