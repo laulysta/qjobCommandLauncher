9fb5ab66a7c4ff6c3df067b92ff00d4f8b27fcac
Xavier Bouthillier <xavier.bouthillier@umontreal.ca>
2017-10-16 13:41:48 -0400

    Set properly account in verify_cedar
    The tests were failing because the account was not specified

--- smartdispatch/tests/verify_cedar.py
+++ smartdispatch/tests/verify_cedar.py
@@ -1,3 +1,4 @@
+import os
 import sys
 
 from verify_slurm_cluster import VerifySlurmCluster, set_defaults
@@ -11,13 +12,17 @@ class VerifyCedarCluster(VerifySlurmCluster):
 
     def get_arguments(self, **kwargs):
 
-        set_defaults(
-            kwargs,
-            coresPerCommand=1,
-            gpusPerCommand=0,
-            walltime=self.WALLTIME,
-            coresPerNode=self.CORES_PER_NODE,
-            gpusPerNode=self.GPUS_PER_NODE)
+        kwargs = super(VerifyCedarCluster, self).get_arguments(**kwargs)
+
+        if kwargs["gpusPerCommand"] == 0:
+            account = os.environ.get("CPU_SLURM_ACCOUNT")
+        else:
+            account = os.environ.get("GPU_SLURM_ACCOUNT")
+
+        if "sbatchFlags" not in kwargs or len(kwargs["sbatchFlags"]) == 0:
+            kwargs["sbatchFlags"] = "--account=" + account
+        else:
+            kwargs["sbatchFlags"] += " --account=" + account
 
         return kwargs
 
